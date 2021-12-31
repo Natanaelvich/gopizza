8a398f00bbe169e7361ac8b086be6f2ea6adad57
Natanael Lima <taelima1997@gmail.com>
2021-12-30 23:54:53 -0300

    add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,31 @@
-import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { Text, View } from 'react-native'
-import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
-import AppLoading from 'expo-app-loading'
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './theme';
 
-export default function Main() {
-	const [fontsLoaded] = useFonts({
-		DMSans_400Regular,
-		DMSerifDisplay_400Regular,
-	})
+const Main: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  });
 
-	if (!fontsLoaded) {
-		return <AppLoading />
-	}
-	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<StatusBar style='auto' />
-			<Text style={{ fontFamily: 'Inter_900Black', fontSize: 40 }}>
-				Inter Black
-			</Text>
-		</View>
-	)
-}
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ThemeProvider theme={theme}>
+        <StatusBar style="auto" />
+        <Text style={{ fontFamily: 'Inter_900Black', fontSize: 40 }}>
+          Inter Black
+        </Text>
+      </ThemeProvider>
+    </View>
+  );
+};
+
+export default Main;
