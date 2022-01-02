a012a0f139236f8c4b6c0842f76269840987e7fa
Natanael Lima <taelima1997@gmail.com>
2022-01-01 23:39:06 -0300

    update signin screen with navigation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import Input from '@/components/Input';
 
 import * as S from './styles';
@@ -6,6 +7,12 @@ import Button from '@/components/Button';
 import { images } from '@/assets';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    navigation.navigate('Home');
+  };
+
   return (
     <S.Container>
       <S.Logo source={images.pizzaSignin} />
@@ -15,7 +22,7 @@ const SignIn: React.FC = () => {
       <Input placeholder="E-mail" />
 
       <S.ForgotPassword>Esqueci minha senha</S.ForgotPassword>
-      <Button title="ENTRAR" />
+      <Button title="ENTRAR" onPress={handleLogin} />
     </S.Container>
   );
 };
