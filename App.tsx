8080beb1c5a35c085ad25ca2eb7ddf17257faaf2
Natanael Lima <taelima1997@gmail.com>
2022-01-02 11:15:24 -0300

    update app root

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Main from './src'
+import 'react-native-gesture-handler';
+import React from 'react';
+import Main from './src';
 
-export default function App() {
-	return <Main />
+export default function App(): JSX.Element {
+  return <Main />;
 }
