3a8b5ac20bcca9ab8198443490b621cf3647edef
Natanael Lima <taelima1997@gmail.com>
2022-01-02 11:14:06 -0300

    update with signOut

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,25 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
+import { useAuth } from '@/hooks/useAuth';
 
 import * as S from './styles';
 import Menu from './_components/Menu';
 import SearchMenu from './_components/SearchMenu';
 
 const Home: React.FC = () => {
+  const { signOut } = useAuth();
+
   return (
     <S.Container>
       <S.Header>
         <S.WrapperTitleAndButtonExit>
           <S.Title>😁 Olá, Garçom</S.Title>
-          <MaterialIcons name="logout" size={24} color="white" />
+          <MaterialIcons
+            name="logout"
+            size={24}
+            color="white"
+            onPress={signOut}
+          />
         </S.WrapperTitleAndButtonExit>
 
         <SearchMenu />
