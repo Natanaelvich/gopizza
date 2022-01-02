35aa452eb03d4e8c4cf7c219f26556d5e3b2ec6e
Natanaelvich <taelima1997@gmail.com>
2022-01-01 22:21:56 -0300

    update index main

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './theme';
+import SignIn from './screens/SignIn';
 
 const Main: React.FC = () => {
   const [fontsLoaded] = useFonts({
@@ -17,17 +17,10 @@ const Main: React.FC = () => {
     return <AppLoading />;
   }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <ThemeProvider theme={theme}>
-        <StatusBar style="auto" />
-        <Text style={{ fontFamily: 'DMSerifDisplay_400Regular', fontSize: 24 }}>
-          DMSerifDisplay_400Regular
-        </Text>
-        <Text style={{ fontFamily: 'DMSans_400Regular', fontSize: 24 }}>
-          DMSans_400Regular
-        </Text>
-      </ThemeProvider>
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar style="auto" />
+      <SignIn />
+    </ThemeProvider>
   );
 };
 
