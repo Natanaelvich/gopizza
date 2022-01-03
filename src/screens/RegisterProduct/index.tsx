8d7c53518b53caaa928b9b68915e3142cf949e8a
Natanael Lima <taelima1997@gmail.com>
2022-01-03 00:00:37 -0300

    add screen regiser product

---
+++ src/screens/RegisterProduct/index.tsx
@@ -0,0 +1,56 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react';
+import Input from '@/components/Input';
+
+import * as S from './styles';
+import Photo from './_components/Photo';
+import InputPrice from './_components/InputPrice';
+
+const RegisterProduct: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.ButtonBack>
+          <MaterialIcons name="keyboard-arrow-left" size={24} color="white" />
+        </S.ButtonBack>
+        <S.Title>Cadastrar</S.Title>
+
+        <S.ButtonDelete>
+          <S.ButtonDeleteText>Deletar</S.ButtonDeleteText>
+        </S.ButtonDelete>
+      </S.Header>
+
+      <S.WrapperUpload>
+        <Photo />
+
+        <S.ButtonUpload title="Carregar" type="secondary" />
+      </S.WrapperUpload>
+
+      <S.WrapperForm>
+        <S.WrapperLabels>
+          <S.LabelInput>Nome</S.LabelInput>
+        </S.WrapperLabels>
+
+        <Input />
+
+        <S.WrapperLabels>
+          <S.LabelInput>Descrição</S.LabelInput>
+
+          <S.LabelMaxCharsInput>Max 60 caracteres</S.LabelMaxCharsInput>
+        </S.WrapperLabels>
+        <Input multiline />
+
+        <S.WrapperLabels>
+          <S.LabelInput>Descrição</S.LabelInput>
+        </S.WrapperLabels>
+
+        <InputPrice />
+        <InputPrice />
+        <InputPrice />
+        <InputPrice />
+      </S.WrapperForm>
+    </S.Container>
+  );
+};
+
+export default RegisterProduct;
---
+++ src/screens/RegisterProduct/styles.ts
@@ -0,0 +1,84 @@
+import { LinearGradient, LinearGradientProps } from 'expo-linear-gradient';
+import styled from 'styled-components/native';
+import Button from '@/components/Button';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+export const Header = styled(LinearGradient).attrs(
+  ({ theme }) =>
+    ({
+      colors: theme.COLORS.GRADIENT,
+      end: { x: 0, y: 0 },
+      start: { x: 1, y: 1 },
+    } as LinearGradientProps),
+)`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 64px 24px 32px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.TITLE};
+  font-size: 24px;
+  color: ${({ theme }) => theme.COLORS.TITLE};
+`;
+
+export const ButtonBack = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+
+  border: 1px solid rgba(255, 255, 255, 0.24);
+  border-radius: 12px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonDelete = styled.TouchableOpacity``;
+
+export const ButtonDeleteText = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.TEXT};
+  font-size: 14px;
+
+  color: #ffffff;
+`;
+
+export const WrapperUpload = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 32px 46px;
+`;
+
+export const ButtonUpload = styled(Button)`
+  width: 90px;
+`;
+
+export const WrapperForm = styled.View`
+  padding: 0 24px;
+`;
+
+export const WrapperLabels = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 12px;
+`;
+
+export const LabelInput = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.TEXT};
+  font-size: 14px;
+
+  color: #572d31;
+`;
+
+export const LabelMaxCharsInput = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.TEXT};
+  font-size: 10px;
+
+  color: #572d31;
+`;
+
+export const ButtonRegister = styled(Button)``;
