38d5f9cb173f3ce031aa6897a5ccce79edf67927
Natanael Lima <taelima1997@gmail.com>
2022-01-11 23:53:42 -0300

    update search menu with fucntion and input props

--- src/screens/Home/_components/SearchMenu/index.tsx
+++ src/screens/Home/_components/SearchMenu/index.tsx
@@ -1,19 +1,25 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
+import { TextInputProps } from 'react-native';
 
 import * as S from './styles';
 
-const SearchMenu: React.FC = () => {
+type Props = {
+  handleSearch: () => void;
+  handleClear: () => void;
+} & TextInputProps;
+
+const SearchMenu: React.FC<Props> = ({ handleSearch, handleClear, ...res }) => {
   return (
     <S.Container>
       <S.WrapperInput>
-        <S.Input />
-        <S.ClearInput>
+        <S.Input {...res} />
+        <S.ClearInput onPress={handleClear}>
           <MaterialIcons name="close" size={16} color="black" />
         </S.ClearInput>
       </S.WrapperInput>
 
-      <S.ButtonSearch>
+      <S.ButtonSearch onPress={handleSearch}>
         <MaterialIcons name="search" size={24} color="white" />
       </S.ButtonSearch>
     </S.Container>
