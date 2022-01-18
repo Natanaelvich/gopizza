e9ff393ee33a6422ee62082def5943a7cf6d8601
Natanael Lima <taelima1997@gmail.com>
2022-01-17 23:17:10 -0300

    remve shadow and background wrapper image

--- src/components/ProductImage/styles.ts
+++ src/components/ProductImage/styles.ts
@@ -2,9 +2,7 @@ import { ImageProps } from 'react-native';
 import styled from 'styled-components/native';
 
 export const WrapperImage = styled.View`
-  elevation: 12;
   overflow: hidden;
-  background: ${({ theme }) => theme.COLORS.SHAPE};
 `;
 
 export const Image = styled.Image.attrs({
